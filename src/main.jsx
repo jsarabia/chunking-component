621e47d7a07b91f66d176b49a68234b66618b522
Maxim <murmax82@gmail.com>
2023-01-06 18:16:10 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import Chunk from "./component/chunk"
 import { book_usx } from "./data";
 
 const root = document.getElementById('chunkingComponent');
-root.setAttribute("data-usx", book_usx);
 
 ReactDOM.createRoot(document.getElementById('chunkingComponent')).render(
   <React.StrictMode>
